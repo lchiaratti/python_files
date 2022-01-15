2406058d14fbe18bee36785920070e6c4772ee8c
Lucas Chiaratti <chiaratti@gmail.com>
2022-01-15 09:57:28 -0300

    7

---
+++ importar.py
@@ -0,0 +1,14 @@
+from abc import ABC  #abstract base classes
+
+from collections.abc import MutableSequence
+
+from numbers import Complex
+
+#class Playlist(MutableSequence):
+    #pass
+class Numero(Complex):
+    def __getitem__(self, item):
+        super().__getitem__()
+    
+#filmes = Playlist()
+filmes = Numero()
--- modelo.py
+++ modelo.py
@@ -42,23 +42,61 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {programa.likes} Likes'
     
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+        #super().__init__(programas)
+    
+    def __getitem__(self, item):
+        return self._programas[item]
+    
+    @property
+    def listagem(self):
+        return self._programas
+
+    #@property
+    def __len__(self):
+        return len(self._programas)
+    
+ #   def tamanho(self):
+ #      return len(self.programas)
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
 #print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
 #      f' - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
-
-atlanta = Serie('atlanta', 2018, 2)
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 #rint(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
 #      f' - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
 
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playslist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:    
+print(f'Tamanho da Playlist: {len(playslist_fim_de_semana)}')
+
+print(vingadores in playslist_fim_de_semana)
+
+#print(playslist_fim_de_semana[0])
+
+
+#for programa in filmes_e_series:
+for programa in playslist_fim_de_semana:    
     #detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
     #if hasattr(programa, 'duracao'):
     #    detalhes = programa.duracao
@@ -68,3 +106,5 @@ for programa in filmes_e_series:
     #print(f'Nome: {programa.nome} - {detalhes} - Likes: {programa.likes}')
     #programa.imprime()
     print(programa)
+
+print(f'Tá ou não tá? {demolidor in playslist_fim_de_semana}')
